58f0eafc4110bce2400b26a3244d7a36554103bf
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 01:59:35 +0300

    feat: implement create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { fileURLToPath } from 'url';
+import { writeFile } from 'fs/promises';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+  try {
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const name = 'fresh.txt';
+    const dest = path.resolve(__dirname, './files', name);
+    const data = 'I am fresh and young';
+
+    await writeFile(dest, data, { flag: 'wx' });
+  } catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await create();
