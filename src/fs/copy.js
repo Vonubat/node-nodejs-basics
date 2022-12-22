7f093dfd3b60819d1d565cc850e2f5cc33c23313
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 02:27:36 +0300

    feat: implement copy.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import { cp } from 'fs/promises';
+import path from 'path';
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = path.resolve(__dirname, './files');
+    const dest = path.resolve(__dirname, './files-copy');
+
+    await cp(src, dest, { recursive: true, force: false, errorOnExist: true });
+  } catch (error) {
+    throw new Error('FS operation failed');
+  }
 };
 
 await copy();
