7f9bb049c5434f0f0c0cff21effe8e1a1c09f83c
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 03:29:50 +0300

    feat: implement delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,17 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { rm } from 'fs/promises';
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = path.resolve(__dirname, './files/fileToRemove.txt');
+
+    await rm(src);
+  } catch (error) {
+    // console.log(error);
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
