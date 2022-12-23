a33ec1b17a106ff69dbd73c531a0b23292a4d4ff
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 10:05:39 +0300

    feat: implement wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,47 @@
+import { fileURLToPath } from 'url';
+import { Worker } from 'worker_threads';
+import path from 'path';
+import os from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const workerDest = path.resolve(__dirname, './worker.js');
+  const cpus = os.cpus().length;
+
+  let increment = 10;
+  const result = [];
+
+  const createWorker = () => {
+    return new Promise((resolve, reject) => {
+      const worker = new Worker(workerDest, {
+        workerData: { n: increment++ },
+      });
+
+      worker.on('message', (value) => {
+        result.push({ status: 'resolved', value: value });
+        resolve(result);
+      });
+
+      worker.on('error', () => {
+        result.push({ status: 'error', value: null });
+        resolve(result);
+      });
+
+      worker.on('exit', (code) => {
+        if (code !== 0) {
+          reject(new Error(`Worker stopped with exit code ${code}`));
+        }
+      });
+    });
+  };
+
+  for (let i = 0; i < cpus; i++) {
+    createWorker().then(() => {
+      if (result.length === cpus) {
+        console.log(result);
+      }
+    });
+  }
 };
 
 await performCalculations();
