860bc422a9afd5162ccb5e31e629f72857e924a0
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 19:03:37 +0300

    refactor: cli tasks

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,9 +1,11 @@
+const PREFIX = '--';
+
 const parseArgs = () => {
-  const cliArgs = process.argv;
+  const cliArgs = process.argv.slice(2);
   const parsedArgs = [];
 
   for (let i = 0; i < cliArgs.length; i++) {
-    const isFlag = cliArgs[i].startsWith('--');
+    const isFlag = cliArgs[i].startsWith(PREFIX);
     if (isFlag) {
       const arg = `${cliArgs[i].slice(2)} is ${cliArgs[i + 1]}`;
       parsedArgs.push(arg);
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,8 @@
+const PREFIX = 'RSS_';
+
 const parseEnv = () => {
   const envVars = Object.entries(process.env);
-  const rssVars = envVars.filter((env) => env[0].startsWith('RSS_'));
+  const rssVars = envVars.filter((env) => env[0].startsWith(PREFIX));
   const output = rssVars.map((value) => value.join('=')).join('; ');
   console.log(output);
 };
