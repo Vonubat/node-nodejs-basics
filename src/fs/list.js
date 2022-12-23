dab60e03edef7706e467e96da614f2e2a7df6d80
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 03:34:14 +0300

    feat: implement list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { readdir } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = path.resolve(__dirname, './files');
+
+    const files = await readdir(src);
+    for (const file of files) {
+      console.log(file);
+    }
+  } catch (error) {
+    // console.log(error);
+    throw new Error('FS operation failed');
+  }
 };
 
 await list();
