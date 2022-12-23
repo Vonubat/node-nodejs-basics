03db7e2570343852810c05b685dfcffe763fccf2
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 03:16:19 +0300

    feat: implement rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { rename as renameFile, access } from 'fs/promises';
+
+const exists = async (path) => {
+  try {
+    await access(path);
+    return true;
+  } catch {
+    return false;
+  }
+};
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    const __dirname = fileURLToPath(new URL('.', import.meta.url));
+    const src = path.resolve(__dirname, './files/wrongFilename.txt');
+    const dest = path.resolve(__dirname, './files/properFilename.md');
+
+    const isExist = await exists(dest);
+    if (isExist) {
+      throw isExist;
+    }
+
+    await renameFile(src, dest);
+  } catch (error) {
+    // console.log(error);
+    throw new Error('FS operation failed');
+  }
 };
 
 await rename();
