bb21fec99cc56474570a4616930ee8c7c335f859
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 08:37:34 +0300

    feat: implement read.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const read = async () => {
-    // Write your code here 
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const src = path.resolve(__dirname, './files/fileToRead.txt');
+
+  const readable = createReadStream(src);
+  readable.pipe(stdout);
 };
 
 await read();
