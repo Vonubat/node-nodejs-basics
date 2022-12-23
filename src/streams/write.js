b6e6c818edad7fb81603204660e5b3d443d0797b
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 08:41:35 +0300

    feat: implement write.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,14 @@
+import { createWriteStream } from 'fs';
+import { stdin } from 'process';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
 const write = async () => {
-    // Write your code here 
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const src = path.resolve(__dirname, './files/fileToWrite.txt');
+
+  const writable = createWriteStream(src);
+  stdin.pipe(writable);
 };
 
 await write();
