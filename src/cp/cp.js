9163bed2bd680025a10cfdbddb74ea3665b37e8c
Tabunov Egor <Vonubat@gmail.com>
2022-12-23 16:30:59 +0300

    featL implement cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,20 @@
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { fork } from 'child_process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __dirname = fileURLToPath(new URL('.', import.meta.url));
+  const src = path.resolve(__dirname, './files/script.js');
+
+  const child = fork(src, args);
+
+  child.on('error', (err) => {
+    throw new Error(err);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(
+  [1, 2, 3]
+  /* [someArgument1, someArgument2, ...] */
+);
